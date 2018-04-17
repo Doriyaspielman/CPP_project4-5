2db9a0be828c5f35a8f3ddcaa8871d7b75a319a6
noytvili <33618257+noytvili@users.noreply.github.com>
2018-04-17 17:10:48 +0300

    Create CircularInt.cpp

---
+++ CircularInt.cpp
@@ -0,0 +1,150 @@
+#include "CircularInt.hpp"
+#include <iostream>
+using namespace std;
+
+CircularInt:: CircularInt(int s, int e){ //constructor
+    start = s;  
+    end = e;
+    cur = start;
+}
+
+//----------------------------------
+// operator ?=
+//----------------------------------
+
+CircularInt& CircularInt::operator+=(int num){
+    int y, sum =0 ;
+    y = end-start+1;
+    sum = (cur+num) % y;
+    if(sum < start){
+        cur = sum+y;
+    }
+    else{
+        cur = sum;
+    }
+        return *this;
+}
+
+CircularInt& CircularInt::operator-=(int num){
+    int y, sum =0 ;
+    y = end-start+1;
+    sum = (cur-num) % y;
+    if(sum < start){
+        cur = sum+y;
+    }
+    else{
+        cur = sum;
+    }
+        return *this;
+}
+
+//----------------------------------
+// operator ??
+//----------------------------------
+
+CircularInt& CircularInt::operator++(){// prefix
+    int y;
+    y = end-start+1;
+    if(cur+1 >= end){
+        cur = (cur+1) % y;
+    }
+    else{
+       cur++;
+    }
+    return *this;
+}
+
+const CircularInt CircularInt::operator++(int){ //postfix
+    CircularInt cpy(*this); 
+    int y;
+    y = end-start+1;
+    if(cur+1 >= end){
+        cur = (cur+1) % y;
+    }
+    else{
+       cur++;
+    }
+    return cpy;
+}
+
+
+ CircularInt& CircularInt::operator--(){//prefix
+    int y;
+    y = end-start+1;
+    if(cur-1 <= start){
+        cur = (cur-1) + y;
+    }
+    else{
+       cur--;
+    }
+    return *this;
+}
+
+const CircularInt CircularInt::operator--(int){ //postfix
+    CircularInt cpy(*this); 
+    int y;
+    y = end-start+1;
+    if(cur-1 <= start){
+        cur = (cur-1) + y;
+    }
+    else{
+       cur--;
+    }
+    return cpy;
+}
+
+//----------------------------------
+// operator ?
+//----------------------------------
+CircularInt CircularInt::operator-(){
+    CircularInt cpy(*this); 
+    int ans = cpy.end - cpy.cur;
+    int y = cpy.end-cpy.start+1;
+    if(ans < cpy.start){
+        cpy.cur = ans + y;
+    }
+    else {
+        cpy.cur = ans;
+    }
+    return cpy;
+}
+
+CircularInt& CircularInt::operator-(const int& num){
+    int y, sum =0 ;
+    y = end-start+1;
+    sum = (num - cur) % y;
+    if(sum < start){
+        cur = sum+y;
+    }
+    else {
+        cur = sum;
+    }
+    return *this;
+}
+
+const CircularInt CircularInt:: operator-(const CircularInt& other) { //this hour - hour
+    int y = end-start+1;
+    cur = (cur - other.cur) % y;
+    if(cur < start){
+        cur = cur+y;
+    }
+    return *this; 
+}
+   
+const CircularInt CircularInt:: operator+(const CircularInt& other) { //this hour + hour
+    int y = end-start+1;
+    cur = (cur + other.cur) % y;
+    return *this; 
+} 
+
+const CircularInt CircularInt:: operator*(const CircularInt& other) { //this hour * hour
+    int y = end-start+1;
+    cur = (cur * other.cur) % y;
+    return *this; 
+} 
+
+// const CircularInt CircularInt:: operator/(const CircularInt& other) { //this hour * hour
+//     int y = end-start+1;
+//     cur = (cur / other.cur) % y;
+//     return *this; 
+// } 
